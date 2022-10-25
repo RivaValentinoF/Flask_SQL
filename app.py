6a1e2f1c380c08ae0b0e467c9920308022b2172d
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-10-25 07:00:33 +0000

    incompleto

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 from flask import Flask, render_template, request
 app= Flask(__name__)
 
+import io
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 @app.route('/', methods=['GET'])
 def home():
-     return render_template('search.html')
+     return render_template('home.html')
+
+@app.route('/selezione', methods=['GET'])
+def selezione():
+    scelta = request.args["scelta"]
+    if scelta=="es1":
+        return render_template("uno.html")
+    elif scelta=="es2":
+        return render_template("due.html")
+    elif scelta=="es3":
+        return render_template("tre.html")
+    elif scelta=="es4":
+        return render_template("search.html")
+
+@app.route('/uno', methods=['GET'])
+def es1(): 
+    import pandas as pd
+    import pymssql
+    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS',
+                           user='riva.valentino', password='xxx123##', database='riva.valentino')
+    # invio query al database e ricezione informazioni
+    query = 'SELECT category_name, count(*) as numero_prodotti FROM production.products inner join production.categories on categories.category_id = products.category_id group by category_name'
+    dfprodotti = pd.read_sql(query, conn)
 
 
-@app.route('/result', methods=['GET'])
+
+@app.route('/results', methods=['GET'])
 def result():
     # collegamneto al database
     import pandas as pd
