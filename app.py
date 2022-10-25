552225853c911dcabfd88241a4b24d35aaf0d3a3
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-10-25 21:46:47 +0000

    es 1 completo

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, render_template, request
-app= Flask(__name__)
-
-import io
-import contextily
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import contextily
+import io
+from flask import Flask, render_template, request,Response
+
+app = Flask(__name__)
+
+matplotlib.use('Agg')
+
 
 @app.route('/', methods=['GET'])
 def home():
-     return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/selezione', methods=['GET'])
 def selezione():
+    global df1 
     scelta = request.args["scelta"]
-    if scelta=="es1":
-        return render_template("uno.html")
-    elif scelta=="es2":
-        return render_template("due.html")
-    elif scelta=="es3":
+    if scelta == "es1":
+        import pandas as pd
+        import pymssql
+        conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS',
+                            user='riva.valentino', password='xxx123##', database='riva.valentino')
+        # invio query al database e ricezione informazioni
+        query = 'SELECT category_name, count(*) as numero_prodotti FROM production.products inner join production.categories on categories.category_id = products.category_id group by category_name'
+        df1 = pd.read_sql(query,conn)
+      
+        
+        return render_template("uno.html", nomiColonne=df1.columns.values, dati=list(df1.values.tolist()))
+    elif scelta == "es2":
+     return render_template("due.html")
+    elif scelta == "es3":
         return render_template("tre.html")
-    elif scelta=="es4":
+    elif scelta == "es4":
         return render_template("search.html")
 
-@app.route('/uno', methods=['GET'])
-def es1(): 
-    import pandas as pd
-    import pymssql
-    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS',
-                           user='riva.valentino', password='xxx123##', database='riva.valentino')
-    # invio query al database e ricezione informazioni
-    query = 'SELECT category_name, count(*) as numero_prodotti FROM production.products inner join production.categories on categories.category_id = products.category_id group by category_name'
-    dfprodotti = pd.read_sql(query, conn)
-
-
+@app.route("/graficoes1", methods=["GET"])
+def graficoes1():
+    fig, ax = plt.subplots(figsize = (12,8))
+    fig.autofmt_xdate(rotation=90)
+    ax.bar(df1.category_name, df1.numero_prodotti, color='g')
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+       
 
 @app.route('/results', methods=['GET'])
 def result():
@@ -49,8 +60,8 @@ def result():
     query = f"select * from production.products where product_name like '{nomeprodotto}%'"
     dfprodotti = pd.read_sql(query, conn)
     # visualizzare le informazioni
-   
-    return render_template('result.html', nomiColonne = dfprodotti.columns.values, dati = list(dfprodotti.values.tolist()))
+
+    return render_template('result.html', nomiColonne=dfprodotti.columns.values, dati=list(dfprodotti.values.tolist()))
 
 
 if __name__ == '__main__':
