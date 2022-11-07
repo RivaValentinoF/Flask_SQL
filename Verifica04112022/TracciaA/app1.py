559a1c77a8be9f8276c69dfdb61a835e37c478bf
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-11-07 08:04:22 +0000

    Sistemato

--- Verifica04112022/TracciaA/app1.py
+++ Verifica04112022/TracciaA/app1.py
@@ -1,35 +1,32 @@
-#Realizzare un sito web cyhe permetta di visualizzare tutti i dipendenti che lavorano in un certo store. 
-# Il maneger inserisce il nome dello store e clicca su un bottone che invia i dati al server. 
-# Quest'ultimo accede al database e restituisce i nomi e i cognomi dei dipendenti di quello store.
-#  Se il nome dello store non è presente, deve essere restituito un opportuno messaggio di errore. 
-# Tutta la parte grafica deve essere gestita con Bootstrap.
-
 from flask import Flask, render_template, request, Response
-
-import pandas as pd
-import pymssql 
-#pip install -U pip
-#pip install pymssql
-
 app = Flask(__name__)
 
+import io
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import pymssql
 
-@app.route('/', methods=['GET'])
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS',
+                           user='riva.valentino', password='xxx123##', database='riva.valentino')
+
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('home1.html')
+    return render_template("home1.html")
 
-@app.route('/ricerca', methods=['GET'])
+@app.route("/ricerca", methods=["GET"])
 def ricerca():
-    NomeStore = request.args['NomeStore']
-    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS',user='riva.valentino', password='xxx123##', database='riva.valentino')
-    query = f"Select sf.first_name, sf.last_name, from sales.stores inner as st join sales.staffs as sf on sf.store_id =st.store_id where st.store_name=  '{NomeCliente}' "
-    df = pd.read_sql(query, conn)
-    
-    return render_template('risultati1.html',nomiColonne=df.columns.values, dati=lis(df.values.tolist()))
-
-
-
-
+    nomeStore = request.args["NomeStore"]
+    query = f"SELECT sf.first_name, sf.last_name FROM sales.stores as st INNER JOIN sales.staffs as sf ON st.store_id = sf.store_id WHERE st.store_name = '{nomeStore}'"
+    tabella = pd.read_sql(query,conn)
+    print(tabella)
+    if tabella.values.tolist() == []:
+        return render_template("error.html")
+    else:
+        return render_template("risultati1.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
 if __name__ == '__main__':
- app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)
